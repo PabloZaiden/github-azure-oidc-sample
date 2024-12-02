f32d2aeaf0945045e03608f32dfde32a2e762f24
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2024-12-02 16:39:02 -0500

    put env back

--- create_entra_app.sh
+++ create_entra_app.sh
@@ -14,6 +14,12 @@ if [ -z "$GITHUB_REPOSITORY_NAME" ]; then
     exit 1
 fi
 
+# Verify if GITHUB_ENVIRONMENT_NAME is set
+if [ -z "$GITHUB_ENVIRONMENT_NAME" ]; then
+    echo "GITHUB_ENVIRONMENT_NAME is not set."
+    exit 1
+fi
+
 echo "Read the current subscription"
 subscriptionId=$(az account show --query id -o tsv)
 
@@ -29,13 +35,13 @@ echo "AZURE_CLIENT_ID=$appId"
 echo "AZURE_TENANT_ID=$tenantId"
 echo "AZURE_SUBSCRIPTION_ID=$subscriptionId"
 
-echo "Configuring federated identity for Github Actions, based on repo name and main branch"
-echo "Replace the subject with the desired subject for the federated credential (environment, branch, tags, pull-request: https://learn.microsoft.com/en-us/entra/workload-id/workload-identity-federation-create-trust?pivots=identity-wif-apps-methods-azcli#github-actions-example)"
+echo "Configuring federated identity for Github Actions, based on repo name and environment name"
+echo "Replace the subject with the desired subject for the federated credential (branch, tags, pull-request: https://learn.microsoft.com/en-us/entra/workload-id/workload-identity-federation-create-trust?pivots=identity-wif-apps-methods-azcli#github-actions-example)"
 parameters=$(cat <<EOF
 {
     "name": "sample-app-credential",
-    "issuer": "https://token.actions.githubusercontent.com/",
-    "subject": "repo:$GITHUB_REPOSITORY_NAME:ref:refs/heads/main",
+    "issuer": "https://token.actions.githubusercontent.com",
+    "subject": "repo:$GITHUB_REPOSITORY_NAME:environment:$GITHUB_ENVIRONMENT_NAME",
     "description": "Sample app credential for Github Actions",
     "audiences": [
         "api://AzureADTokenExchange"
