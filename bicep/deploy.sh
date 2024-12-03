40a91ba8c11246f64f7ff82c6c7a8ddd872276ab
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2024-12-02 19:14:54 -0500

    bicep

---
+++ bicep/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Read the resource group name from the first argument
+resourceGroupName=$1
+
+# Check if the resource group name is provided.
+if [ -z "$resourceGroupName" ]; then
+    echo "Resource group name is required."
+    exit 1
+fi
+echo "Resource group name: $resourceGroupName"
+
+# Read the location from the second argument
+location=$2
+
+# Check if the location is provided.
+if [ -z "$location" ]; then
+    echo "Location is required."
+    exit 1
+fi
+
+# deploy with bicep main.bicep and main.bicepparam
+RESOURCE_GROUP_NAME=$resourceGroupName LOCATION=$location az deployment sub create --location $location --template-file main.bicep --parameters main.bicepparam
---
+++ bicep/main.bicepparam
@@ -0,0 +1,4 @@
+using 'main.bicep'
+
+param resourceGroupName = readEnvironmentVariable('RESOURCE_GROUP_NAME')
+param location = readEnvironmentVariable('LOCATION')
